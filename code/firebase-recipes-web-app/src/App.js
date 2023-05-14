696133eb2460310ae9fb9b3d389ac7ffc972aa79
naavni <132861981+naavni@users.noreply.github.com>
2023-05-13 21:50:12 -0700

    initial commit

--- code/firebase-recipes-web-app/src/App.js
+++ code/firebase-recipes-web-app/src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
 import firebase from './FirebaseConfig';
 
 function App() {
   return (
     <div className="App">
-    <div className="title-row">
-      <h1 className="title">Firebase Recipes</h1>
-    </div>
+      <div className="title-row">
+        <h1 className="title">Firebase Recipe</h1>
+        </div>
     </div>
   );
 }
